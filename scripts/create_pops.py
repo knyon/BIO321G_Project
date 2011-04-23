81c305e35a11a6de55e5faaab45888223005c5ce
Lane Smith <lane.smith@me.com>
2011-04-23 12:52:54 -0500

    Script complete. Need to bug check.

--- scripts/create_pops.py
+++ scripts/create_pops.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # File Name : scripts/create_pops.py
 # Creation Date : Wed Apr 20 14:10:41 2011
-# Last Modified : Sat Apr 23 12:13:39 2011
+# Last Modified : Sat Apr 23 12:52:06 2011
 # Created By :  Lane Smith
 
 import os, shutil
@@ -11,6 +11,9 @@ EVN = "environment.cfg"
 CUSTOM_EVNS = ["control_environment.cfg", "treatment_environment.cfg"]  # Required configuration files for this script
 AVIDA_DIR = path.join(os.pardir, 'avida') # Relative location of the avida directory
 
+
+# Verify the location of the avida directory, the avida executable, and the
+# required files for this script.
 def verify_file_locs(required_files):
     if not path.exists(AVIDA_DIR):
         print "Script has not been run in the right location. Please run this in a directory whose parent directory contains the directory 'avida'."
@@ -31,39 +34,62 @@ def run_avida(cfg, seed):
     os.system("./avida -s " + str(seed) + " > "+os.devnull)  # Execute avida with the given seed. Throw output into the bit bucket.
     os.rename(EVN, cfg)
 
+
+# Move a file 'src' to directory 'dest_dir'. If directory does not exist,
+# create a directory. Add an underscore and an incrementing number if there is
+# a naming conflict at the destination.
 def move_file(src, dest_dir):
+
+    if not path.exists(src):
+        print "The source file you're trying to move doesn't even exist!"
+        exit()
 
     if not path.exists(dest_dir):
         os.mkdir(dest_dir)
 
-    filename, ext = path.splitext(src)
+    src_dir, src_file = path.split(src)
+    filename, ext = path.splitext(src_file)
     end_count = 1
-    while path.exists(path.join(dest_dir, filename+ext):
+    while path.exists(path.join(dest_dir, filename+ext)):
         end_str = str(end_count)
         if filename.endswith("_"+end_str):
-            filename = filename[:-len(end_str)]
+            filename = filename[:-len(end_str)]  #Remove the digits
         else:
             filename += "_"
         filename += end_str
         end_count +=1
 
-    shutil.move(src, path.join(dest_dir, filename+ext)
+    dest = path.join(dest_dir, filename+ext)
+    shutil.move(src, dest)
+    return dest
 
+# Output a message. If the OS is Mac OS X and has growlnotify installed, print
+# the message using growlnotify (which is more noticeable than the terminal)
 def display_msg(msg):
-    if os.name == 'posix' AND path.exists('/usr/local/bin/growlnotify'): # If the OS is Mac OS X and has growlnotify installed, print the message using growlnotify
-        os.system("growlnotify -m '"+msg+"' -s")
+    if os.name == 'posix' and path.exists('/usr/local/bin/growlnotify'): # 
+        os.system("growlnotify -m '"+msg+"' -s") # -s makes it sticky (doesn't go away until clicked)
     else:
         print msg
 
 
 verify_file_locs(CUSTOM_EVNS)
-seed = input("Please enter the seed number: ")
+seed = input("Please enter the seed number (digits of UT EID): ")
+username = raw_input("Please enter your first name: ").lower()
 
-if path.exists(EVN)     # If there already exists an environment.cfg file, move it out of the way
+if path.exists(EVN):  # If there already exists an environment.cfg file, move it out of the way
     os.rename(EVN, "temp")
+
 for cfg in CUSTOM_EVNS:
+    run = 1
     run_avida(cfg)
-    move_file(path.join('data','detail-100000.spop')
-if path.exists("temp"):  # Reset the environment.cfg file
+    display_msg("Run "+str(run) +" of avida completed.")
+
+    data_src = path.join('data','detail-100000.spop')
+    data_dest = path.join(os.pardir, 'saved_data', username)
+    dest = move_file(data_src, data_dest)
+    display_msg("File "+data_src+" moved to "+dest)
+
+
+if path.exists("temp"):  # Put back the environment.cfg file
     os.rename("temp", EVN)
 display_msg("Script has finished.")
