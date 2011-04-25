6f5c849d3ace9bb14863444d9419745baf1d263d
Lane Smith <lane.smith@me.com>
2011-04-24 19:56:21 -0500

    Now can run experiment, not iteratively

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # File Name : scripts/run_experiment.py
 # Creation Date : Sat Apr 23 13:53:22 2011
-# Last Modified : Sun Apr 24 18:37:15 2011
+# Last Modified : Sun Apr 24 19:43:13 2011
 # Created By :  Lane Smith
 
 # This script automatically runs the experiment by using the population files
@@ -43,20 +43,19 @@ for cfg in CUSTOM_EVNS:
 
 with open("data/dom_genotype.dat") as fp:
     for line in fp:
-        if line[0] != "#" and len(line) > 10:
-            dom_genotype = line.strip()
+        if re.match(r'[^#]\w+', line):
             break
-
     
 # edit the events.cfg file
-with open("experiment_environment.cfg") as fp:
+with open("experiment_events.cfg") as fp:
     detail_line = re.compile(r"(.*InjectSequence\s)(\w+)")
-    fo = open("environment.cfg", "w")
+    fo = open("events.cfg", "w")
     for line in fp:
         if detail_line.match(line):
-            line = detail_line.sub(r'\1'+dom_genotype+'\n')
+            line = detail_line.sub(r'\1'+dom_genotype+'\n',line)
         fo.write(line)
     fo.close()
+
 
 
 
