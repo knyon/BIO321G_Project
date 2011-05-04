1099dc61ffe11cda876cb53893bf48eae2e12af7
Lane Smith <lane.smith@me.com>
2011-05-04 11:54:13 -0500

    Move files bug fixed.

--- scripts/create_pops.py
+++ scripts/create_pops.py
@@ -12,25 +12,35 @@ from helper_functions import verify_file_locs, move_files, display_msg, run_avid
 
 EVN = "environment.cfg" 
 CUSTOM_EVNS = ["native_environment.cfg", "invasive_environment.cfg"]  # Required configuration files for this script
+EVENTS = "events.cfg"
+CUSTOM_EVENTS = "create_pop_events.cfg"
 AVIDA_DIR = path.join(os.pardir, 'avida') # Relative location of the avida directory
 
 
-verify_file_locs(CUSTOM_EVNS, AVIDA_DIR)
+verify_file_locs(CUSTOM_EVNS+[CUSTOM_EVENTS], AVIDA_DIR)
 seed = input("Please enter the seed number (digits of UT EID): ")
 username = raw_input("Please enter your first name: ").lower()
 
-move_files("temp", EVN) # If there already exists an environment.cfg file, move it out of the way
+moved_cfgs = move_files("temp", EVN, EVENTS) # If there already exists an environment.cfg or evemts.cfg file, move it out of the way
 
 try:
+    os.rename(CUSTOM_EVENTS, EVENTS)
     for cfg in CUSTOM_EVNS:
+        os.rename(cfg, EVN)
         run = 1
         run_avida(seed)
         display_msg("Run "+str(run) +" of avida completed.")
         run += 1
+        os.rename(EVM, cfg)
 
         data_src = path.join('data','detail-100000.spop')
         data_dest = path.join(os.pardir, 'saved_data', username)
         dest = move_files(data_dest, data_src)
+        if run == 1:
+            os.rename(dest[0],path.join(path.dirname(dest[0]), "native-detail-100000.spop"))
+        if run == 2:           #it shouldn't run more than twice, but I don't want it to fail if it does
+            os.rename(dest[0],path.join(path.dirname(dest[0]), "invasive-detail-100000.spop"))
 finally:
-    move_files(os.curdir, path.join("temp", EVN))
+    os.rename(EVENTS, CUSTOM_EVENTS)
+    move_files(os.curdir, *moved_cfgs)
 display_msg("Script has finished.")
